e65d6c965d9e6aa534b27268ecac7ecff1cbec3e
Your Name <you@example.com>
2021-03-29 22:20:17 +0300

    Теперь обработчики событий реализованы отдельными классами в отдельном файле

--- common.py
+++ common.py
@@ -1,8 +1,8 @@
-from tkinter.filedialog import askopenfilenames, asksaveasfilename
 from parserconfig import *
 from GUI.widgets import *
 from GUI.guimixin import *
 from GUI.guimaker import GuiMakerFrameMenu
+from ActionInterfaces import *
 
 
 class Window(GuiMakerFrameMenu):
@@ -37,7 +37,9 @@ class ConfigMixin:
         return dict((str(key), path) for key, path in enumerate(apps, start))
 
 
-class MainWindow(Window, ConfigMixin):
+class MainWindow(Window, ConfigMixin, AddConfigActionInterface,
+                 AppendToConfigActionInteface, DeleteConfigActionInterface, ResetPathActionInterface,
+                 ShortCutActionInterface):
 
     def __init__(self, root=None, config=Config()):
         self.config = config
@@ -63,82 +65,6 @@ class MainWindow(Window, ConfigMixin):
             showerror(ERROR_BAD_PATH_MESSAGE, error_message)
 
 
-    # START MAKE ADD ACTION
-
-    def add_config(self):
-        self.delete_widgets()
-        self.new_config_name = StringVar('')
-        self.new_apps = {}
-        self.make_new_config_form()
-
-    def make_new_config_form(self):
-        entry(self.main_frame, side=TOP, linkvar=self.new_config_name)
-        button(self.main_frame, side=TOP, text=APPS_CONFIG_LABEL,
-               command=lambda: self.new_apps.update(self.validate_apps(askopenfilenames())))
-        button(self.main_frame, side=TOP, text=FORM_SUBMIT_LABEL, command=self.new_config_action)
-
-    def new_config_action(self):
-        try:
-            self.config.add_config(self.new_config_name.get(), self.new_apps)
-        except:
-            showerror(CONFIG_ALREADY_EXIST_ERROR, CONFIG_ALREADY_EXIST_ERROR_MESSAGE)
-            return
-        self.delete_widgets()
-        self.make_widgets()
-
-    # END MAKE ADD ACTION
-
-    # START MAKE APPEND_TO ACTION
-
-    def append_to_config(self):
-        self.delete_widgets()
-        self.new_apps = {}
-        self.make_append_to_config_form()
-
-    def make_append_to_config_form(self):
-        self.list_config_names = listbox(self.main_frame, lines=self.config.configs.keys())
-        button(self.main_frame, side=TOP, text=APPS_CONFIG_LABEL,
-               command=lambda: self.new_apps.update(self.validate_apps(askopenfilenames(), self.get_append_to_index())))
-        button(self.main_frame, side=TOP, text=FORM_SUBMIT_LABEL, command=self.append_to_config_action)
-
-    def append_to_config_action(self):
-        try:
-            self.config.append_to_config(self.get_current_selection_name(), self.new_apps)
-        except:
-            showerror(CONFIG_APPEND_ERROR, CONFIG_APPEND_ERROR_MESSAGE)
-            return
-        self.delete_widgets()
-        self.make_widgets()
-
-    # END MAKE APPEND_TO ACTION
-
-    #START MAKE DELETE ACTION
-
-    def delete_config(self):
-        self.delete_widgets()
-        self.make_delete_config_form()
-
-    def make_delete_config_form(self):
-        self.list_config_names = listbox(self.main_frame, lines=self.config.configs.keys())
-        button(self.main_frame, side=TOP, text=FORM_SUBMIT_LABEL, command=self.delete_config_action)
-
-    def delete_config_action(self):
-        try:
-            self.config.delete_config(self.get_current_selection_name())
-        except:
-            showerror(CONFIG_DELETE_CONFIG_ERROR, CONFIG_DELETE_CONFIG_ERROR_MESSAGE)
-            return
-        self.delete_widgets()
-        self.make_widgets()
-
-    # END MAKE APPEND_TO ACTION
-
-    #START MAKE DELETE ACTION
-
-    def reset_path_apps(self):
-        self.delete_widgets()
-        self.make_reset_path_apps_form()
-
     def on_select(self, e):
         self.config_name = self.get_current_selection_name()
         self.list_apps = listbox(self.main_frame,
@@ -149,38 +75,3 @@ class MainWindow(Window, ConfigMixin):
         self.app_name = self.get_current_app_name()
         app_path = self.config.configs[self.config_name][self.app_name]
         self.current_path = askopenfilename(initialfile=app_path)
-
-    def make_reset_path_apps_form(self):
-        self.list_config_names = listbox(self.main_frame, lines=self.config.configs.keys())
-        self.list_config_names.bind('<<ListboxSelect>>', self.on_select)
-        button(self.main_frame, side=TOP, text=FORM_SUBMIT_LABEL, command=self.reset_path_apps_action)
-
-    def reset_path_apps_action(self):
-        try:
-            self.config.reset_path(self.config_name, self.app_name, self.current_path)
-        except:
-            showerror(CONFIG_RESET_PATH_ERROR, CONFIG_RESET_PATH_ERROR_MESSAGE)
-            return
-        self.delete_widgets()
-        self.make_widgets()
-
-    # END MAKE MAKE_SHORTCUT ACTION
-
-    def make_shortcut(self):
-        self.delete_widgets()
-        self.make_make_shortcut_form()
-
-    def make_make_shortcut_form(self):
-        self.list_config_names = listbox(self.main_frame, lines=self.config.configs.keys())
-        button(self.main_frame, side=TOP, text=FORM_SUBMIT_LABEL, command=self.make_shortcut_action)
-
-    def make_shortcut_action(self):
-
-        self.config.make_shortcut(repr(self.get_current_selection_name()), asksaveasfilename(initialfile=self.get_current_selection_name() + '.py', filetypes=SHORTCUTS_TYPES))
-
-        self.delete_widgets()
-        self.make_widgets()
-
-
-
-    # END MAKE APPEND_TO ACTION
